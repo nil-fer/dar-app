9a52e54c84db4ff09714b472179f96bf4ae8543b
nil-fer <nilmusaeva@gmail.com>
2020-05-26 22:20:56 +0300

    styles added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,20 @@ class ApplicationController < ActionController::Base
 
   # protect_from_forgery with: :exception
 
+  layout :layout_by_resource
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
+  end
+  
   def configure_permitted_parameters
       added_attrs = [:email, :password, :password_confirmation, :remember_me]
       devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
